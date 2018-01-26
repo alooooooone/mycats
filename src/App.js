d3b1dafeacbdbc2a3dd0a4ac7c8ca16c48189979
jielongINK <heyliudiao@gmail.com>
2018-01-26 12:13:44 +0800

    chapter-page页面的编写完成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Count from './components/Count'
+import Photos from './components/Photos'
 import './reset.css'
 import './app.css'
 
@@ -12,28 +13,72 @@ class App extends Component {
 			currentPage: -1,
 			chapterMenu: [
 				{
-					title: 74,
+					title: 9,
 					headline: "可爱的Lucky",
-					bgImage: "https://a.photo/images/2018/01/19/LRG_DSC03195.jpg",
-					color: "pink",
+					bgImage: "https://a.photo/images/2018/01/19/LRG_DSC03412.jpg",
+					color: "linear-gradient(-180deg, rgb(254, 177, 200) 0%, rgb(254, 177, 200) 14%, rgb(101, 167, 255) 40%)",
+					menuColor: "#eb97b4",
+					photos: [
+						"https://a.photo/images/2018/01/21/LRG_DSC03114.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03137.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03145.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03164.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03468.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03481.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03548.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03556.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03559.jpg",
+					]
 				},
 				{
-					title: 85,
+					title: 13,
 					headline: "搞怪的Lucky",
 					bgImage: "https://a.photo/images/2018/01/19/LRG_DSC03544.jpg",
-					color: "pink",
+					color: "linear-gradient(-180deg, rgb(255, 235, 111) 1%, rgb(255, 235, 111) 14%, rgb(255, 119, 119) 39%)",
+					menuColor: "#ffcc48",
+					photos: [
+						"https://a.photo/images/2018/01/21/LRG_DSC03496.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03491.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03474.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03199.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03190.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03189.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03187.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03186.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03182.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03143.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03128.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03119.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03102.jpg",
+					]
 				},
 				{
-					title: 96,
+					title: 8,
 					headline: "严肃的Lucky",
 					bgImage: "https://a.photo/images/2018/01/19/LRG_DSC03505.jpg",
-					color: "pink",
+					color: "linear-gradient(-180deg, rgb(196, 178, 248) 3%, rgb(196, 178, 247) 14%, rgb(255, 158, 158) 36%)",
+					menuColor: "#b29bff",
+					photos: [
+						"https://a.photo/images/2018/01/21/LRG_DSC03506.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03488.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03425.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03423.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03194.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03193.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03150.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03129.jpg",
+					]
 				},
 				{
-					title: 31,
+					title: 2,
 					headline: "丑陋的Lucky",
 					bgImage: "https://a.photo/images/2018/01/19/LRG_DSC03412.jpg",
-					color: "pink",
+					color: "linear-gradient(-180deg, rgb(72, 116, 220) 6%, rgb(73, 116, 220) 13%, rgb(149, 228, 239) 40%)",
+					menuColor: "#4978dc",
+					photos: [
+						"https://a.photo/images/2018/01/21/LRG_DSC03470.jpg",
+						"https://a.photo/images/2018/01/21/LRG_DSC03185.jpg",
+					]
 				},
 			]
 		}
@@ -45,25 +90,48 @@ class App extends Component {
 		})
 	}
 	onMouseOverShowChapterMenuBg = (index) => {
-		return () => {
-			this.setState({
-				hoverItemIndex: index
-			})
+		let { currentPage } = this.state
+
+		if (currentPage === -1){
+			return () => {
+				this.setState({
+					hoverItemIndex: index
+				})
+			}
 		}
 	}
 	onMouseOutResetChapterMenuBg = () => {
-		this.setState({
-			hoverItemIndex: -1
-		})
-	}
-	onClickRedireactToChapter = (index) => {
-		return () => {
+		let { currentPage } = this.state
+
+		if(currentPage === -1){
 			this.setState({
-				currentPage: index
+				hoverItemIndex: -1
 			})
 		}
 	}
+	onClickRedireactToChapter = (index) => {
+		let { currentPage } = this.state
 
+		if (currentPage === -1) {
+			return () => {
+				this.setState({
+					currentPage: index,
+					hoverItemIndex: -1
+				})
+			}
+		}
+	}
+	onClickChangePage = (index) => {
+		if(index !== this.state.currentPage){
+			document.documentElement.scrollTop = 0;
+
+			return () => {
+				this.setState({
+					currentPage: index
+				})
+			}
+		}
+	}
 	componentDidMount(){
 		var html = document.documentElement;
 
@@ -102,7 +170,7 @@ class App extends Component {
 								</div>
 								<span className="chapter-headline">{item.headline}</span>
 							</a>
-							{index === this.state.chapterMenu.length - 1 ? null : <span className={`separator-default ${pageDisappearFlag && hoverItemIndex === -2 ? "separator-animation" : ""} ${[-1, -2, index].indexOf(hoverItemIndex) === -1 ? "separator-hidden" : ""}  ${hoverItemIndex === index ? "separator-show" : ""}`}></span>}
+							{index === this.state.chapterMenu.length - 1 ? null : <span className={`separator-default ${pageDisappearFlag && hoverItemIndex === -2 ? "separator-animation" : ""} ${[-1, -2, index].indexOf(hoverItemIndex) === -1 || currentPage === index ? "separator-hidden" : ""}  ${hoverItemIndex === index ? "separator-show" : ""}`}></span>}
 						</li>
 					)
 				}
@@ -132,11 +200,9 @@ class App extends Component {
 			<div className="chapter">
 				{
 					chapterMenu.map((item, index) => 
-						<div
-						key={item.title}
-						className={`chapter-page ${currentPage === index ? "chapter-page-show" : ""}`}
-						style={{ backgroundColor: `${item.color}` }}>
-						</div>
+						<Photos 
+						key={item.headline}
+						photos={item.photos} currentPage={currentPage} chapterMenu={chapterMenu} color={item.color} currentIndex={index} changePage={this.onClickChangePage} menuColor={item.menuColor}/>
 					)
 				}
 			</div>
@@ -144,7 +210,7 @@ class App extends Component {
 	}
 
 	render() {
-		let {pageDisappearFlag} = this.state;
+		let {pageDisappearFlag,currentPage} = this.state;
 
 		return (
 			<div className="wrapper">
@@ -160,7 +226,7 @@ class App extends Component {
 						<span className="detail">一只蠢萌的颜值飘忽不定的猫</span>
 					</div>
 				</div>
-				<div className="tip">想看什么样的Lucky?</div>
+				<div className="tip" style={currentPage !== -1 ? {visibility: "hidden"} : {}}>想看什么样的Lucky?</div>
 				{this.renderChapterMenu()}
 				{this.renderChapterMenuBg()}
 				{this.renderChapter()}
---
+++ src/components/Photos.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import "./Photos.css"
+
+class Photos extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            photos: this.props.photos
+        }
+    }
+
+    componentDidUpdate(){
+        let { currentPage, currentIndex } = this.props;
+        
+        if(currentIndex === currentPage){
+            let chapter = document.getElementsByClassName("chapter-page");
+            let items = chapter[currentPage].getElementsByClassName("chapter-page-item")
+            document.addEventListener("scroll", function () {
+                let scrollTop = document.documentElement.scrollTop;
+                let windowHeight = document.documentElement.clientHeight;
+                
+                Array.prototype.forEach.call(items,(item,index)=>{
+                    let itemHeight = item.clientHeight;
+                    let itemOffsetTop = item.offsetTop;
+                    let img = item.children[0];
+                    
+                    if(itemHeight/2 + itemOffsetTop < scrollTop + windowHeight){
+                        let position = parseInt(img.getAttribute("data-index"),10) % 2 === 0 ? "img-show-right" : "img-show-left";
+
+                        img.classList.add(position);
+                    }
+                })
+            })
+        }
+    }
+
+    renderChapterPageItem(){
+        let { photos } = this.props;
+
+        return photos.map((item, index) =>
+            <div key={item} className="chapter-page-item">
+                <img src={item} alt="pic" style={index % 2 === 0 ? { right: "-808px" } : { left: "-808px" }} data-index={index} />
+            </div>
+        )
+    }
+    
+    renderChapterPageMenu(){
+        let { menuColor, currentPage } = this.props;
+
+        return <div className="chapter-page-footer" style={{ backgroundColor: menuColor }}>
+            <ul className="chapter-page-menu clearfix">
+                {
+                    ["可爱的", "搞怪的", "严肃的", "丑陋的"].map((item, index) =>
+                        <li key={item} onClick={this.props.changePage(index)} style={{opacity: index === currentPage ? 1 : 0.5}}>{item}</li>
+                    )
+                }
+            </ul>
+        </div>
+    }
+
+    render() {
+        let { currentPage,color,currentIndex } = this.props;
+
+        return (
+            <div
+                className={`chapter-page ${currentPage === currentIndex ? "chapter-page-show" : ""}`}
+                style={{ backgroundImage: `${color}` }} ref="box">
+                { this.renderChapterPageItem() }
+                { this.renderChapterPageMenu() }
+            </div>
+        )
+    }
+}
+
+export default Photos;
